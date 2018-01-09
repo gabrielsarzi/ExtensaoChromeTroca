f95dd3b196bf27c2a1a52875071152fd2efe0f76
Gabriel Sarzi <gabrielsarzilima@gmail.com>
2018-01-09 14:06:51 -0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,19 @@
+var elements = document.getElementsByTagName('*');
+var troca = ["mão-leve","escamoteador","ladrão","malandro","gatuno","larápio","Diabo","Lúcifer","Demo","Tinhoso","bandido","vigarista","Coisa-ruim","criminoso","Mefistófeles","delinquente","assaltante","perpetrador","salteador"];
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/PT/g, troca[Math.floor(Math.random()*18)]);
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
